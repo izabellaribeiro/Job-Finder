a3892c882a4db22ce255722d95e0c59d711b7a64
Izabella Ribeiro <izabella.ribeirro@gmail.com>
2020-08-26 20:41:37 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000;
+
+app.listen(PORT, function(){
+    console.log(`O Express está rodando na porta ${PORT}`);
+})
+
+app.get('/', (req, res) => {
+    res.send("Está funcionando!");
+})

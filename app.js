f1a6002e308a14c3e050caacd8910c6309debe43
Izabella Ribeiro <izabella.ribeirro@gmail.com>
2020-09-05 10:56:01 -0300

    add search logic

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const path = require('path');
 const db = require('./db/connection'); // banco de dados oferecidos
 const bodyParser = require('body-parser');
 const Job = require('./models/Job');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
 const PORT = 3000;
 
@@ -40,14 +42,34 @@ db
 
 //routes 
 app.get('/', (req, res) => {
-    Job.findAll({order: [
-        ['createdAt', 'DESC']
-    ]})
-    .then(jobs => {
-        res.render('index', {
-            jobs
-        });
-    })
+
+    let search = req.query.job;
+    let query = '%'+search+'%' // PH -> PHP 
+
+    if(!search) {
+        Job.findAll({order: [
+            ['createdAt', 'DESC']
+        ]})
+        .then(jobs => {  
+            res.render('index', {
+                jobs
+            });
+        })
+        .catch(err => console.log(err))
+    } else {
+        Job.findAll({
+            where: {title: {[Op.like]: query}}, 
+            order: [
+            ['createdAt', 'DESC']
+        ]})
+        .then(jobs => {
+            res.render('index', {
+                jobs,search
+            });
+        })
+    }
+
+    
 })
 
 //jobs routes 
